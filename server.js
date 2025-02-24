342506326a9466f7bbafe4f439540308f144da5a
Sahil Pawar <165409638+sahil261005@users.noreply.github.com>
2025-02-24 16:31:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,4 +27,7 @@ const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("JobDhundo Backend is Running!");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
