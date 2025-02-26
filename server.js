8591b9ec47cd1715a0d21eaca041ec74d8d46bc6
Sahil Pawar <165409638+sahil261005@users.noreply.github.com>
2025-02-26 22:14:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
