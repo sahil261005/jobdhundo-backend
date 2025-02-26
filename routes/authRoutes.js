0390b5676ce6bb5efd5dfe566da1e517a3e14935
Sahil Pawar <165409638+sahil261005@users.noreply.github.com>
2025-02-26 19:37:07 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,8 +14,9 @@ router.post("/register", async (req, res) => {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "User already exists" });
 
-    // Hash password and create new user
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     user = new User({ username, email, password: hashedPassword });
     await user.save();
 
