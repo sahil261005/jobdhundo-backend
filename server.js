b0fb3a6332ce185cdf424f0d8c61284ac2107222
Sahil Pawar <165409638+sahil261005@users.noreply.github.com>
2025-02-26 19:33:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(cors({
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
